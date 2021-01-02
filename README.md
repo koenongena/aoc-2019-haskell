7a1f63cb81eb4c0726144c5c7178097dd24988e5
koen.ongena <gijoke@gmail.com>
2021-01-02 11:03:19 +0100

    AoC 2019 - Day 2 - Part 2. Updated README

--- README.md
+++ README.md
@@ -9,5 +9,8 @@ Install stack: https://docs.haskellstack.org/en/stable/README/
 
 Run the individual day solutions using stack. The interpreter is specified in the hs files. For example
 
+    stack runghc -- -isrc app/Day2.hs
+
+If the Day doesn not use any methods defined in src/, you can simplify this command:
+
     stack app/Day1.hs
-    
--- app/Day2.hs
+++ app/Day2.hs
@@ -1,11 +1,4 @@
-modifyNth :: Int -> (a -> a) -> [a] -> [a]
-modifyNth _ _ [] = []
-modifyNth n newValFn (x : xs)
-  | n == 0 = newValFn x : xs
-  | otherwise = x : replaceNth (n -1) (newValFn x) xs
-
-replaceNth :: Int -> a -> [a] -> [a]
-replaceNth n newVal = modifyNth n (const newVal)
+import Lib
 
 atHaltCode :: [Int] -> Int -> Bool
 atHaltCode arr i = arr !! i == 99
